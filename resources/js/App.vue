01cc0a43745d91219d4d5c1c88f6b2f7050792ad
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-15 21:03:52 +0100

    premier commit

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div>
+      <h1>Hello, Vue!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  }
+  </script>
+  
+  <style scoped>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+
+  </style>
+  
